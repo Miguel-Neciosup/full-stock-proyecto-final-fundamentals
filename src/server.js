0853d32e1813be873aff53b64963a6a39991edb1
miguel <miguelangelneciosupvelasquez@gmail.com>
2026-02-03 13:04:12 -0500

    primer commit

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "node:url";
+import { readFile } from "node:fs/promises";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.use(express.static("assets"));
+
+async function readJson() {
+  try {
+    const content = await readFile(
+      path.join(__dirname, "categories.json"),
+      "utf8",
+    );
+    return JSON.parse(content);
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
+
+// Handlers
+async function homeHandler(req, res) {
+  const categories = await readJson();
+  res.render("index" , {categories :categories});
+}
+
+async function categoryHandler(req, res) {
+  const categories = await readJson();
+  const categoryId = Number(req.params.id);
+  const category = categories.find((c) => c.id === categoryId);
+  res.render("category", { category: category, products:category.products });
+}
+
+// Router
+app.get("/", homeHandler);
+app.get("/categories/:id", categoryHandler);
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+// Listar todas las categorias
+// Ver detalle de una categoria
